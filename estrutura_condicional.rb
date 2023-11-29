f85d47c5adc45feddb582187c9500e5f99fa161f
pierresouza <pierre.s3@hotmail.com>
2023-11-29 14:42:42 -0300

    update

--- estrutura_condicional.rb
+++ estrutura_condicional.rb
@@ -1,5 +1,7 @@
+
 puts 'digite um numero 1 ou 2'
 v1 = gets.to_i
+
 
 if v1 == 1
    puts 'valor igual a 1'
@@ -7,4 +9,21 @@ elsif v1 == 2
    puts 'valor igual a 2'
 else
    puts ' valor não é igual a 1 ou 2'
+end
+
+
+unless v1 = 2
+   pouts 'condição falsa'   
+else
+    puts 'condição verdadeira'
+end
+
+case v1 
+   when 0
+      puts 'você digitou 0'
+   when 1
+      puts ' você digitou 1'
+   else
+      puts 'opção inválida'
+   
 end
---
+++ vetores.rb
@@ -0,0 +1,32 @@
+
+
+#       0,1,2,3,4
+# vetor[1,2,3,4,5]
+
+
+# hase = {"keys","valor"}
+
+
+# hashes = {"a", => "1", "b" => "1"}
+# hashes[""]
+# hashes = {a, => "1", b => "1"}
+# hashes[:a] 
+
+# vetor = [1,2,7,8,9,3,4,5]
+
+# vetor.each do |i|
+#    puts i
+# end
+
+
+# (1...5).each do |i|
+#    puts i
+#         
+# 
+# end
+
+# (1..5).each do |i|
+#    puts i
+# end
+
+5.times{puts "bruno"}
