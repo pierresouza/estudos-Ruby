74514cbdd1f417bfac272cffb7510ad26a220834
pierresouza <pierre.s3@hotmail.com>
2023-11-30 14:59:42 -0300

    create new items

--- cucumberteste/tests/features/step_definitions/primeiro_cenario.rb
+++ cucumberteste/tests/features/step_definitions/primeiro_cenario.rb
@@ -1,1 +1,7 @@
+Quando('eu somo {int} {float} {int}') do |valor1, float, valor2|
+@soma = valor1 + valor2
+end
 
+Entao('o resultado tem que ser {int}') do |resultado|
+   expect(@soma).to eq resultado
+end
